6562d620a0d47bc5c4cb07a3b458f009761daafa
Conghao Xiong <conghao.xiong@gmail.com>
2020-11-10 20:07:42 +0800

    Finish writing web crawler

--- extract_new_words/crawlbaidupedia.py
+++ extract_new_words/crawlbaidupedia.py
@@ -4,18 +4,18 @@ import json
 
 from selenium import webdriver
 
-def get_info_box(driver):
+def get_infobox(driver):
 	if driver.find_elements_by_class_name("basic-info") == []:
 		return {}
 	left = driver.find_element_by_class_name("basic-info").find_element_by_class_name("basicInfo-left")
 	right = driver.find_element_by_class_name("basic-info").find_element_by_class_name("basicInfo-right")
 	left_entry, left_answer = left.find_elements_by_tag_name("dt"), left.find_elements_by_tag_name("dd")
 	right_entry, right_answer = right.find_elements_by_tag_name("dt"), right.find_elements_by_tag_name("dd")
-	info_box = {}
+	info_box = []
 	for entry, answer in zip(left_entry, left_answer):
-		info_box[entry.text] = answer.text
+		info_box.append({"value": answer.text, "key": entry.text})
 	for entry, answer in zip(right_entry, right_answer):
-		info_box[entry.text] = answer.text
+		info_box.append({"value": answer.text, "key": entry.text})
 	return info_box
 
 def get_subtitles(driver):
@@ -42,11 +42,11 @@ def get_content(driver):
 def get_web_info(driver):
 	title = driver.find_element_by_class_name('lemmaWgt-lemmaTitle-title').find_element_by_tag_name("h1").text
 	summary = driver.find_element_by_class_name('lemma-summary').find_elements_by_class_name("para")
-	summary = [each.text for each in summary]
-	info_box = get_info_box(driver)
-	subtitles = get_subtitles(driver)
-	content = get_content(driver)
-	return title, summary, info_box, subtitles, content
+	summary = "".join([each.text for each in summary])
+	infobox = get_infobox(driver)
+	# subtitles = get_subtitles(driver)
+	# content = get_content(driver)
+	return {"abs": summary, "name": title, "infobox": infobox}
 
 
 if __name__ == "__main__":
@@ -55,23 +55,32 @@ if __name__ == "__main__":
 	found = []
 	driver = webdriver.Chrome()
 
-
-	with open("words.txt", "r") as f:
+	with open("word_reliable.txt", "r") as f:
 		for line in f:
 			words.append(line.strip())
 
 	with open("baidu.json", "w", encoding="utf-8") as f:
-
 
 		for each in words:
 			url = "https://baike.baidu.com/item/" + each
 			driver.get(url)
 			if driver.find_elements_by_class_name('wiki-lemma') == []:
 				didnt_find.append(each)
+				json.dump({"name":each}, f, ensure_ascii=False)
+				f.write("\n")
 				continue
 			found.append(each)
 			json.dump(get_web_info(driver), f, ensure_ascii=False)
 			f.write("\n")
 			time.sleep(2)
-
+	
 	driver.close()
+
+	assert len(found) + len(didnt_find) == len(words)
+	
+	with open("found_words.txt", "w") as f:
+		for each in found:
+			f.write(each + "\n")
+	with open("didnt_find.txt", "w") as f:
+		for each in didnt_find:
+			f.write(each + "\n")
